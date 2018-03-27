9dd710e6fc0a65ba66e42daadae9209c82bc612e
Jayenne Montana <jayenne@hotmail.com>
2018-03-27 14:23:47 +0100

    Fixed the previous fix, properly. Then tweaked the defaults a little.

--- src/hrblock-block/component.js
+++ src/hrblock-block/component.js
@@ -5,17 +5,17 @@ mbrApp.loadComponents(
 			_params:{
 				group1: {type:'separator',title: 'Horizontal Rule'},
 				hrColor:{type:"color",title:"Color",default:"#000"},
-                hrThickness:{type:"range",title:"Thickness(px)",min:0,max:10,step:1,default:1},
+                hrThickness:{type:"range",title:"Thickness(px)",min:0,max:15,step:1,default:3},
 				hrFullWidth:{type:"switch",title:"Fullscreen",default:false},
 				hrWidth:{type:"range",title:"Width(%)",min:0,max:100,step:1,default:100,condition:["!hrFullWidth"]},
-				hrStyleSolid:{type:"radio",title:"Solid",name:"hrstyle",default:!0},
+				hrStyleSolid:{type:"radio",title:"Solid",name:"hrstyle",default:!1},
 				hrStyleDotted:{type:"radio",title:"Dotted",name:"hrstyle",default:!1},
-                hrStyleDashed:{type:"radio",title:"Dashed",name:"hrstyle",default:!1},
-                paddingTop:{type:"range",title:"Padding(px)",min:0,max:80,step:10,default:1},
+                hrStyleDashed:{type:"radio",title:"Dashed",name:"hrstyle",default:!0},
+                paddingTop:{type:"range",title:"Padding(px)",min:0,max:80,step:10,default:10},
                 bgColor:{type:"color",title:"Background Color",default:"#fff"},
 			},
             hrFullWidth:".hrfullwidth {min-width:0;}",
-			hrWidth:".hrblock {width:100%}",
+			hrWidth:".hrblock {width:100%;margin:0;}",
 			paddingTop:".block {padding-bottom:0; padding-top:0;}",
 			_onParamsChange:function(c,a,b){
 				
