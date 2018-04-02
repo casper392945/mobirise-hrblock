b9fae1fd9b53b7079312535cae4c77e0bbdfa64a
Jayenne Montana <jayenne@hotmail.com>
2018-04-02 12:16:46 +0100

    refactored for flexability

--- src/hrblock-block/component.js
+++ src/hrblock-block/component.js
@@ -2,9 +2,20 @@ mbrApp.loadComponents(
 	"hrblock",
 	{"hrblock-block":{
 			_group:"Article",
-			_onParamsShow: function(e,$params,$block) {
-				this._params.hrAnchor = this._anchor;							
-			},
+			
+			_onLoadFromProject : function(originalComponent){
+                //alert(this._params.divtag);
+                this.col = '#f0f';
+            },
+			
+			hrColor:"border-color:#000;",
+			hrThickness:"border-top-width:3px;",
+            hrFullWidth:"min-width:0;",
+			hrWidth:"width:100%;",
+			hrStyle:"border-style:dashed;",
+			hrPaddingBlock:"padding-bottom:10px; padding-top:10px;",
+			hrDrawStyle:"",
+
 			_params:{
 				group1: {type:'separator',title: 'Horizontal Rule'},
 				hrColor:{type:"color",title:"Color",default:"#000"},
@@ -16,47 +27,44 @@ mbrApp.loadComponents(
                 hrStyleDashed:{type:"radio",title:"Dashed",name:"hrstyle",default:!0},
                 hrPaddingBlock:{type:"range",title:"Padding(px)",min:0,max:80,step:10,default:10},
                 bgColor:{type:"color",title:"Background Color",default:"#fff"},
-                hrAnchor:{type:"text",title:"anchor",default:this._anchor},
-
+                hrAnchor:{type:"text",title:"anchor",default:this._anchor}
 			},
 			
-			hrColor:".hrcolor {border-color:#000;}",
-			hrThickness:".hrthickness {border-top-width:3px;}",
-            hrFullWidth:".hrfullwidth {min-width:0;}",
-			hrWidth:"hr.hrblock {width:100%;padding:0;}",
-			hrStyle:".hrstyle {border-style:dotted;}",
-			hrPaddingBlock:".block {padding-bottom:10px; padding-top:10px;}",
-			
+			_onParamsShow: function(e,$params,$block) {
+			},
+					
 			_onParamsChange:function(c,a,b){
 				
 				"hrFullWidth"==a&&b==true&&(
-					this.hrFullWidth=".hrfullwidth {min-width:100%;}"
+					this.hrFullWidth="min-width:100%;"
 				);
 				"hrFullWidth"==a&&b==false&&(
-					this.hrFullWidth=".hrfullwidth {min-width:0%;}"
+					this.hrFullWidth="min-width:0%;"
 				);
 				"hrWidth"==a&&(
-					this.hrWidth=".hrblock {width:"+b+"%;}"
+					this.hrWidth="width:"+b+"%;"
 				);
 				"hrColor"==a&&(
-					this.hrColor=".hrcolor {border-color:"+b+";}"
+					this.hrColor="border-color:"+b+";"
 				);
 				"hrStyleSolid"==a&&(
-					this.hrStyle=".hrstyle {border-style:solid;}"
+					this.hrStyle="border-style:solid;"
 				);
 				"hrStyleDotted"==a&&(
-					this.hrStyle=".hrstyle {border-style:dotted;}"
+					this.hrStyle="border-style:dotted;"
 				);
 				"hrStyleDashed"==a&&(
-					this.hrStyle=".hrstyle {border-style:dashed;}"
+					this.hrStyle="border-style:dashed;"
 				);
 				"hrThickness"==a&&(
-					this.hrThickness=".hrthickness {border-top-width:"+b+"px;}"
+					this.hrThickness="border-top-width:"+b+"px;"
 				);
 				"hrPaddingBlock"==a&&(
-					this.hrPaddingBlock=".block {padding-bottom:"+b+"px; padding-top:"+b+"px;}"
+					this.hrPaddingBlock="padding-bottom:"+b+"px; padding-top:"+b+"px;"
 				);
+				this.hrDrawStyle=this.hrFullWidth+this.hrWidth+this.hrColor+this.hrStyle+this.hrThickness;
 			},
+			
 			_styles: {
 				'hr': {
 					'margin': '0'
